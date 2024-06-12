2c345da3cc30134553d73c6ca490b2ed07871cdd
amine omari <omari1amineco@gmail.com>
2024-06-12 20:43:19 +0100

    feat: add addTask function

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
+
+function addTask() {
+  if (inputBox.value === "") {
+    alert("you must write something!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+  }
+}
